ffef9817e9115f05a847ab293985dead72d92d69
ali elgendy <alie57217@gmail.com>
2025-11-25 18:23:55 +0200

    NavBar Styles

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,36 +1,30 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { NavDropdown } from 'react-bootstrap';
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import { NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FaHeartbeat } from "react-icons/fa";
+import { IoMdArrowDropdownCircle } from "react-icons/io";
+import "./NavBar.css";
 
 function NavBar() {
   return (
-    //  <Navbar bg="dark" variant="dark" expand="lg">
-    //   <Container>
-    //     <Navbar.Brand as={Link} to="/">🏥 My Hospital</Navbar.Brand>
-    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //     <Navbar.Collapse id="basic-navbar-nav">
-    //       <Nav className="me-auto">
-    //         <Nav.Link as={Link} to="/">Home</Nav.Link>
-    //         <Nav.Link as={Link} to="/departments">Departments</Nav.Link>
-    //         <Nav.Link as={Link} to="/doctors/orthopedics">Doctors</Nav.Link>
-    //         <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-    //       </Nav>
-    //     </Navbar.Collapse>
-    //   </Container>
-    // </Navbar>
-    
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar expand="lg" className="navbar">
       <Container>
-        <Navbar.Brand as={Link} to="/">🏥 My Hospital</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          <FaHeartbeat /> Medic
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/departments">Departments</Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/departments">
+              Departments
+            </Nav.Link>
 
             {/* Doctors nested under Departments */}
             <NavDropdown title="Doctors" id="doctors-dropdown">
@@ -46,9 +40,16 @@ function NavBar() {
               {/* Add more departments as needed */}
             </NavDropdown>
 
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/contact">
+              Contact
+            </Nav.Link>
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
           </Nav>
+          <Button as={Link} to="/departments" className="btn-get-started">
+            Get Started
+          </Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
